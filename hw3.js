d754e1fe68bb2f58eb570b58d9db202867205416
Francisco Guijarro <franleplant@gmail.com>
2014-04-01 16:44:15 -0300

    Create hw3.js

---
+++ hw3.js
@@ -0,0 +1,7 @@
+//Using streams
+
+//Require de "fs" module, which comes with node's native STD library
+var fs = require('fs');
+
+//Read the content of hello_world.txt and stream it out.
+fs.createReadStream('hello_world.txt').pipe(process.stdout);
